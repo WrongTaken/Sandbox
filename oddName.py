59ba3d0429bd3b86798d4fafdad29d5711dcc879
WrongTaken <45630569+WrongTaken@users.noreply.github.com>
2019-08-14 19:13:28 +0900

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,15 @@
 """Lee Jungwoo Practical 3"""
+name_list = []
+Number = int(input("How many people are you going to input?"))
+for i in range(Number):
+    valid_input = False
+    name = input("Please, input the name")
+    while valid_input == False:
+        if " " in name:
+            print("Do not input blank")
+            name = input("Please, input the name")
+        else:
+            name_list.append(name)
+            valid_input = True
+for j in range(Number):
+    print(name_list[j][1])
